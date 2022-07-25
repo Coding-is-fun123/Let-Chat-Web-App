93385f213c348f4a06550d95d1f3cfec1efff05b
Coding-is-fun123 <99051307+Coding-is-fun123@users.noreply.github.com>
2022-07-25 14:22:08 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHuumDdTw2KELHbnV2tLaWT3Zemiug10A",
+    authDomain: "let-chat-web-app-f771f.firebaseapp.com",
+    projectId: "let-chat-web-app-f771f",
+    storageBucket: "let-chat-web-app-f771f.appspot.com",
+    messagingSenderId: "977110867697",
+    appId: "1:977110867697:web:952597fd50619e45475350"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+
+  
